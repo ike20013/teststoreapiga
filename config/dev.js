af4a750e09466bc61ae4b184d7e81ccc625349bf
krinjmann <tavklandre@gmail.com>
2020-11-03 15:48:00 +0300

    change gitignore

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mongoURI: 'mongodb://127.0.0.1:27017',
+    jwtSecret: '5B34C0874DC93FCC390E40FF617B4AB12271DDBA64C7F2A833FDB64BF78F7AA1',
+    stripeSecret: 'pk_test_51HjCrpEbteUUuPkN7lgPzkmBTgOoxt2YLVfHHtSiYsW9iOqY1P0NoJ9s9KdHEBqYc19dVhjhOR7GBdZlKGHmabeb00GfEunSRk',
+    endpointSecret: 'whsec_TYbVfxJftVbpWOk0bHl9C7utYfWBqw8I',
+    s3SecretAccessKey: 'Cyk8RH1zxTXmZOcvzDsk9RCGHPTxj+9l9L8QO05W',
+    s3AccessKeyId: 'AKIAIEDTE3Z5KC2JBVUQ',
+    s3PublicBucket: 'arn:aws:s3:eu-central-1:450680851633:accesspoint/publicaccess',
+    s3PrivateBucket: 'arn:aws:s3:eu-central-1:450680851633:accesspoint/privateaccess'
+}
+
+// 11
